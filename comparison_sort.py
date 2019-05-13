1ac56f01eee0658b9ec10ab0e049675460e6ecab
Christopher Specht <cfspecht@users.noreply.github.com>
2019-05-13 15:59:27 -0700

    wrote 80% of bubble sort

--- comparison_sort.py
+++ comparison_sort.py
@@ -7,6 +7,19 @@ Due Friday, May 17th
 """
 
 def bubble_sort(alist):
+    """ Sorts integers using bubble sort, takes n passes
+    Author: Chris Specht
+    Args:
+        alist (list):
+    """
+    for sortedlist in range(len(alist) - 1, 0, -1): # always takes n passes
+        for i in range(len(sortedlist)):
+            if alist[i] > alist[i + 1]:
+                alist[i], alist[i + 1] = alist[i + 1], alist[i]
+    return alist
+
+
+def bubble_sort2(alist):
     """
     Author: Chris Specht
     """
