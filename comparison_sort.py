da0fa5dccc5df76a35183e221932e84ba05ae374
Christopher Specht <cfspecht@users.noreply.github.com>
2019-05-13 15:35:42 -0700

    create sort file

---
+++ comparison_sort.py
@@ -0,0 +1,31 @@
+""" Lab 6: Sorting Algorithms
+Chris Specht
+Koichi Kodama
+CPE 202-09
+Spring 2019
+Due Friday, May 17th
+"""
+
+
+def bubble_sort(alist):
+    pass
+
+
+def insertion_sort(alist):
+    pass
+
+
+def selection_sort(alist):
+    pass
+
+
+def quick_sort(alist):
+    pass
+
+
+def merge_sort(alist):
+    pass
+
+
+def heap_sort(alist):
+    pass
