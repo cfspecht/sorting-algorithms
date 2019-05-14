b6433f82dea8c7ffb424ef49b9cb23b8a296a62d
kmkodama <50600194+kmkodama@users.noreply.github.com>
2019-05-13 19:58:59 -0700

    Update comparison_sort.py

--- comparison_sort.py
+++ comparison_sort.py
@@ -54,7 +54,8 @@ def bubble_sort2(alist): # TESTED, works
 
 def insertion_sort(alist):
     """Sorts a list through Insertion sort
-
+    Author: 
+        Koichi Kodama
     Args:
         alist (list): List to be sorted through
     Returns:
@@ -85,11 +86,26 @@ def selection_sort(alist):
     pass
 
 
-def quick_sort(alist):
+def partition(arr,low,high): 
+    i = ( low-1 ) 
+    pivot = arr[high] 
+  
+    for j in range(low , high):
+        if   arr[j] <= pivot: 
+            i = i+1 
+            arr[i],arr[j] = arr[j],arr[i] 
+    arr[i+1],arr[high] = arr[high],arr[i+1] 
+    return ( i+1 ) 
+
+
+def quickSort(arr,low,high):
     """
     Author: Koichi Kodama
     """
-    pass
+    if low < high:
+        pi = partition(arr,low,high) 
+        quickSort(arr, low, pi-1) 
+        quickSort(arr, pi+1, high)
 
 
 # TODO add comparison counter to this once toshi answers question
